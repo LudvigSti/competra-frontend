beb3bc40d018cad8986cac372f8cdbf281d246a6
John Ferdie Abueg <john.abueg@gmail.com>
2025-01-23 09:20:24 +0100

    registration implementation

--- competra-frontend/src/App.jsx
+++ competra-frontend/src/App.jsx
@@ -4,12 +4,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
 			</Routes>
 		</>
 	);
